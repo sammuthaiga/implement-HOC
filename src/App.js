6a8cf3aa46e24d1a6a3c16955fb3bce72e52f537
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-07-31 23:29:28 +0300

    implement mouse position tracking using a HOC

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {useState, useEffect} from "react";
+
+
+const withMousePosition = (WrappedComponent) => {
+  return (props) => {
+    const[mousePosition, setMousePosition] = useState({
+      x: 0,
+      y: 0,
+    })
+
+    useEffect(() => {
+      const handleMousePositionChange = (e) => {
+        setMousePosition({
+          x: e.clientX,
+          y: e.clientY,
+        });
+      };
+
+      window.addEventListener('mousemove', handleMousePositionChange);
+
+      return () => {
+        window.removeEventListener('mousemove', handleMousePositionChange);
+      }
+    }, []);
+    return <WrappedComponent {...props} mousePosition={mousePosition}/>
+  }
+}
+
+const PanelMouseLogger = ({ mousePosition}) => {
+  if (!mousePosition) {
+    return null;
+  }
+  return (
+    <div className="BasicTracker">
+      <p>Mouse Position</p>
+      <div className="Row">
+        <span>x: {mousePosition.x}</span>
+        <span>y: {mousePosition.y}</span>
+      </div>
+    </div>
+  );
+};
+
+const PointMouseLogger = ({ mousePosition }) => {
+  if (!mousePosition) {
+    return null;
+  }
+
+  return (
+    <p>
+      {mousePosition.x}, {mousePosition.y}
+    </p>
+  )
+}
+
+const PanelMouseTracker = withMousePosition(PanelMouseLogger)
+const PointMouseTracker = withMousePosition(PointMouseLogger)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <header className=' header'>Little Lemon Restaurant</header>
+      <PanelMouseTracker />
+      <PointMouseTracker />
     </div>
   );
 }
